2ece2d6ae8eadd2cacb5687dc11d7904f27b6989
Pavani Ayenampudi <pavani@Pavanis-MacBook-Air.local>
2017-07-09 23:26:14 -0700

    newFile

---
+++ src/test/java/projectMaven/TestAccount.java
@@ -0,0 +1,19 @@
+package projectMaven;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Created by pavani on 7/9/17.
+ */
+public class TestAccount {
+    @Test
+
+    public void newTC()
+
+    {
+        Assert.assertEquals(1,2);
+
+    }
+
+}
